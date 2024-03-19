b5201a2810400d02f2cbc9d1383851f28f751119
Adi Bustan <dididi101@gmail.com>
2024-03-20 01:04:30 +0200

    fix problems with the profile updates

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -26,6 +26,21 @@ class userController extends BaseController<IUser>{
         req.body.phone = user.phone;
         super.post(req, res);
     }
+
+    async updateById (req: AuthResquest, res: Response) {
+        console.log("updateEventById: " + req.body._id)
+        try {
+            const model = await user_model.findByIdAndUpdate(req.body._id, 
+                {   username: req.body.username,
+                    phone: req.body.phone,
+                    imgName: req.body.imgName,});
+            res.status(200).send(model);
+            console.log("seccess")
+        } catch(err) {
+            console.log(err)
+            res.status(500).json({message: err.message});
+        }
+    }
 }
 
 export default new userController();
